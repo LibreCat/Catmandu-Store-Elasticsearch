6f6dc3fa297191d736355bda57d06fe4c43baa0a
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2012-12-04 14:26:23 +0100

    version

--- lib/Catmandu/Store/ElasticSearch.pm
+++ lib/Catmandu/Store/ElasticSearch.pm
@@ -13,11 +13,11 @@ Catmandu::Store::ElasticSearch - A searchable store backed by ElasticSearch
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
