c0d679d884d20192e73b46e70decde6f3ac6851b
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2016-08-23 13:33:31 +0200

    0.0501

--- lib/Catmandu/Store/ElasticSearch/CQL.pm
+++ lib/Catmandu/Store/ElasticSearch/CQL.pm
@@ -2,7 +2,7 @@ package Catmandu::Store::ElasticSearch::CQL;
 
 use Catmandu::Sane;
 
-our $VERSION = '0.05';
+our $VERSION = '0.0501';
 
 use Catmandu::Util qw(require_package trim);
 use CQL::Parser;
--- lib/Catmandu/Store/ElasticSearch/Searcher.pm
+++ lib/Catmandu/Store/ElasticSearch/Searcher.pm
@@ -2,7 +2,7 @@ package Catmandu::Store::ElasticSearch::Searcher;
 
 use Catmandu::Sane;
 
-our $VERSION = '0.05';
+our $VERSION = '0.0501';
 
 use Moo;
 use namespace::clean;
