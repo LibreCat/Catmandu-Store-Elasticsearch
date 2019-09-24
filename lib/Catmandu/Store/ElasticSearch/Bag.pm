cac822f406c5214de2a546079917e53a02b258d8
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2019-09-24 14:54:49 +0200

    1.02

--- lib/Catmandu/Store/ElasticSearch/Bag.pm
+++ lib/Catmandu/Store/ElasticSearch/Bag.pm
@@ -2,7 +2,7 @@ package Catmandu::Store::ElasticSearch::Bag;
 
 use Catmandu::Sane;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 use Catmandu::Hits;
 use Cpanel::JSON::XS qw(encode_json decode_json);
